18ffb73c869863c6fd8d56017fbb5860c89f8d5c
Neeraj Sharma <nitsnwits@gmail.com>
2015-04-26 19:18:52 -0700

    error page

--- config/init/routes.js
+++ config/init/routes.js
@@ -14,5 +14,13 @@ module.exports = function(app, env) {
 	// User routes
 	app.post(baseurl + '/users', userController.postUser);
 	app.get(baseurl + '/users/:user_id', userController.getUser);
-	
+	app.post(baseurl + '/login', userController.postLogin);
+
+	// errors
+	app.get(baseurl + '/errors', errorController.getErrors);
+	app.get(baseurl + '/errors.json', errorController.getErrorsJson);
+
+	// cpu status
+	app.get(baseurl + '/cpu', errorController.getCpu);
+	app.get(baseurl + '/cpu.json', errorController.getCpuJson);
 }
